c55e5151356b2c8fc2aba2246263efa764b12463
Darshan Hegde <darshan.p.hegde@gmail.com>
2018-10-22 13:32:01 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -41,4 +41,5 @@ echo " Install mysql server"
 echo "mysql-server-5.7 mysql-server/root_password password root" | sudo debconf-set-selections
 echo "mysql-server-5.7 mysql-server/root_password_again password root" | sudo debconf-set-selections
 apt-get -y install mysql-server-5.7
+apt-get -y install mysql-server-5.7
 echo "Install mysql-server done"
